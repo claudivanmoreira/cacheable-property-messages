a4c511d273af47c5ebe039239cc8e77ffb41bf1d
Claudivan Moreira <claudivan.ifpb@gmail.com>
2017-03-28 21:06:19 -0300

    Version Initial Of Implementation

--- src/main/java/br/com/ceosites/cacheablepropertymessages/cache/PropertiesConfigurationCache.java
+++ src/main/java/br/com/ceosites/cacheablepropertymessages/cache/PropertiesConfigurationCache.java
@@ -1,64 +1,70 @@
 package br.com.ceosites.cacheablepropertymessages.cache;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.concurrent.ConcurrentMap;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.configuration.ConfigurationException;
 import org.apache.commons.configuration.PropertiesConfiguration;
-import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
 
-import br.com.ceosites.cacheablepropertymessages.io.PropertiesIterable;
 import br.com.ceosites.cacheablepropertymessages.io.PropertiesReader;
 
 public class PropertiesConfigurationCache {
 
 	private static Logger LOGGER = LoggerFactory.getLogger(PropertiesConfigurationCache.class);
-	private PropertiesConfigurationCacheLoader cacheLoader;
 	private PropertiesReader propertiesReader;
-	private PropertiesConfiguration configuration;
 	private LoadingCache<String, String> cache;
-	private final Long DEFAULT_CACHE_SIZE = 1200L;
-	private final Long DEFAULT_CACHE_TIMEOUT_SECONDS = 3600L;
-	private final Long DEFAULT_CACHE_TIME_TO_LIVE_SECONDS = 300L;
-	private Long maximumSize;
-	private Long timeOutSeconds;
-	private Long timeToLiveSeconds;
-
-	public PropertiesConfigurationCache(PropertiesConfigurationCacheLoader cacheLoader, Long maximumSize, Long timeToLiveSeconds, Long timeOutSeconds) {
-		this.cacheLoader = cacheLoader;
-		this.maximumSize = maximumSize == null ? DEFAULT_CACHE_SIZE : maximumSize;
-		this.timeOutSeconds = timeOutSeconds == null ? DEFAULT_CACHE_TIMEOUT_SECONDS : timeOutSeconds;
-		this.timeToLiveSeconds = timeToLiveSeconds == null ? DEFAULT_CACHE_TIME_TO_LIVE_SECONDS : timeToLiveSeconds;
+	
+	public PropertiesConfigurationCache(PropertiesReader propertiesReader) {
+		this.propertiesReader = propertiesReader;
 		buildCache();
 	}
-
+	
 	private void buildCache() {
-		LOGGER.debug("Cache configurations received: cacheLoader=<{}>, maximumSize <{}>, timeToLiveSeconds <{}>, timeOutSeconds<{}>.", new Object[]{cacheLoader, maximumSize, timeToLiveSeconds, timeOutSeconds});
-		this.cache = CacheBuilder.newBuilder().maximumSize(maximumSize).expireAfterAccess(timeToLiveSeconds, TimeUnit.SECONDS)
-				.expireAfterWrite(timeOutSeconds, TimeUnit.SECONDS).build(cacheLoader);
-	}
-
-	public String getCachedProperty(String key) {
-		try {
-			if (configuration == null) {
-				configuration = propertiesReader.getAllProperties();
-			}
-			return this.cache.getAll(new PropertiesIterable(configuration)).get(key);
-		} catch (Exception e) {
-			LOGGER.error("Fail on trying to get value for the key {} from cache. Error Message: {}", key, e.getMessage());
-			return StringUtils.EMPTY;
-		}
+		cache = CacheBuilder.newBuilder().recordStats().maximumSize(4)
+				.expireAfterAccess(4, TimeUnit.SECONDS)
+				.expireAfterWrite(4,  TimeUnit.SECONDS)
+				.build(new CacheLoader<String, String>(){
+			
+			private PropertiesConfiguration configuration;
+			
+			@Override
+			public String load(String key) throws Exception {
+				System.out.println("Cahamando load....");
+				return getMap().get(key);
+			}			
+		});
 	}
 	
-	public ConcurrentMap<String, String> getCacheAsMap() {
-		return cache.asMap();
+	public String get(String key) throws Exception {
+		System.out.println("Cahamando get....");
+		if (cache.asMap().containsKey(key)) {
+			return cache.getUnchecked(key);
+		} else {
+			cache.putAll(getMap());
+			return cache.getUnchecked(key);
+		}
+		
+	}
+	
+	public Map<String,String> getMap() throws ConfigurationException {
+		System.out.println("Chamando getMap...");
+		PropertiesConfiguration configuration = propertiesReader.getAllProperties();
+		Map<String, String> map = new HashMap<String, String>();
+		Iterator<String> it = configuration.getKeys();
+		while (it.hasNext()) {
+			String key = it.next();
+			map.put(key, configuration.getString(key));
+		}
+		return map;
 	}
 
 }
--- src/test/java/br/com/ceosites/cacheablepropertiesmessages/cache/test/PropertiesConfigurationCacheTest.java
+++ src/test/java/br/com/ceosites/cacheablepropertiesmessages/cache/test/PropertiesConfigurationCacheTest.java
@@ -1,99 +1,35 @@
 package br.com.ceosites.cacheablepropertiesmessages.cache.test;
 
-import static org.hamcrest.CoreMatchers.not;
-
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
 
-import org.apache.commons.configuration.ConfigurationException;
-import org.hamcrest.CoreMatchers;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 
-import com.google.common.cache.CacheBuilder;
-import com.google.common.cache.LoadingCache;
-
 import br.com.ceosites.cacheablepropertymessages.cache.PropertiesConfigurationCache;
-import br.com.ceosites.cacheablepropertymessages.cache.PropertiesConfigurationCacheLoader;
 import br.com.ceosites.cacheablepropertymessages.io.PropertiesReader;
-import cache.MessagesCache;
 
 public class PropertiesConfigurationCacheTest {
 
 	private PropertiesReader propertiesReader;
-	private PropertiesConfigurationCacheLoader cacheLoader;
 	private PropertiesConfigurationCache propertiesCache;
 	
 	@Before
 	public void setup() {
-//		propertiesReader = new PropertiesReader(null, new String[]{"cacheable-files.properties"});
-//		cacheLoader = new PropertiesConfigurationCacheLoader(propertiesReader);
-//		propertiesCache = new PropertiesConfigurationCache(cacheLoader, null, null, null);
+		propertiesReader = new PropertiesReader(null, new String[]{"cacheable-files.properties"});
+		propertiesCache = new PropertiesConfigurationCache(propertiesReader);
 	}
 
 	@Test
-	@Ignore
 	public void testPropertiesReader() {
 		try {
-			not(propertiesReader.getAllProperties().isEmpty());
-			not(propertiesReader.getAllProperties().isEmpty());
-		} catch (ConfigurationException e) {
-			e.printStackTrace();
-		}
-	}
-
-	@Test
-	@Ignore
-	public void testCacheLoader() {
-		try {
-			CoreMatchers.notNullValue().matches(cacheLoader.load("background.color"));
-			CoreMatchers.notNullValue().matches(cacheLoader.load("page.width"));
-			CoreMatchers.notNullValue().matches(cacheLoader.load("background.color"));
-			CoreMatchers.notNullValue().matches(cacheLoader.load("page.width"));
+			System.out.println(propertiesCache.get("text.color"));
+			System.out.println(propertiesCache.get("page.width"));
+			Thread.sleep(4000);
+			System.out.println(propertiesCache.get("text.color"));
+			System.out.println(propertiesCache.get("page.width"));
+			System.out.println(propertiesCache.get("text.color2"));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-
-	@Test
-	@Ignore
-	public void testBuildCache() {
-		LoadingCache<String, String> cache = CacheBuilder.newBuilder()
-				.maximumSize(5)
-				.expireAfterAccess(5, TimeUnit.SECONDS)
-				.expireAfterWrite(10, TimeUnit.SECONDS).build(cacheLoader);
-		try {
-			System.out.println(cache.get("page.width"));
-			System.out.println(cache.get("page.width"));
-		} catch (ExecutionException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	@Test
-	public void testValueCachedProperty() throws InterruptedException {
-		try {
-			MessagesCache cache = MessagesCache.buildCache();
-			System.out.println("Primeira execucao...");
-			System.out.println(cache.getKey("page.width"));
-			System.out.println(cache.getKey("background.color"));
-			System.out.println("Segunda execucao...");
-			System.out.println(cache.getKey("page.width"));
-			System.out.println(cache.getKey("background.color"));
-			Thread.sleep(3000);
-			System.out.println("Terceira execucao...");
-			System.out.println(cache.getKey("page.width"));
-			System.out.println(cache.getKey("background.color"));
-			System.out.println("Quarta execucao...");
-			System.out.println(cache.getKey("page.width"));
-			System.out.println(cache.getKey("background.color"));
-		} catch (ConfigurationException e) {
-			e.printStackTrace();
-		} catch (ExecutionException e) {
-			e.printStackTrace();
-		}
-		
-	}
-
 }
